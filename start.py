361aa827c0da982556d707b6b39b6ef70e2f169b
axelsvenn <mrr.axelsven@gmail.com>
2022-04-16 16:58:15 +0300

    req

--- start.py
+++ start.py
@@ -10,20 +10,47 @@ def start(update, context):
     update.message.reply_text(
         "Здравствуйте! Для начала работы этого бота...\n"
         "...требуется ввести ваши личные данные.\n"
-        "Введите ваш номер карты.")
+        "Как я могу к вам обращаться?")
+    return 0
+
+
+def null_response(update, context):
+    name = update.message.text  # name
+    update.message.reply_text(
+        "Так-с, теперь введите данные вашей карты.\n"
+        "Начнём с номера карты. Введите её в формате:\n"
+        "xxxx xxxx xxxx xxxx")
     return 1
 
 
 def first_response(update, context):
     locality = update.message.text  # номер карты
-    update.message.reply_text(f"Какая погода в городе {locality}?")
+    update.message.reply_text("Хорошо, теперь введите дату истечения срока карты!\n"
+                              "Формат карты месяца(M) и года(Y):\n"
+                              "MM/YY")
     return 2
 
 
 def second_response(update, context):
-    weather = update.message.text
-    print(weather)
-    update.message.reply_text("Спасибо за участие в опросе! Всего доброго!")
+    locality = update.message.text  # дата истечения
+    update.message.reply_text("Хорошо, теперь введите ваши...\n"
+                              "...имя и фамилию по паспорту.\n"
+                              "Формат ввода: GRIGORIY IVANOV")
+    return 3
+
+
+def third_response(update, context):
+    locality = update.message.text  # имя собственное
+    update.message.reply_text("Ну и последнее - введите...\n"
+                              "...число на задней стороне карты.\n"
+                              "Формат ввода: xxx")
+    return 4
+
+
+def forth_response(update, context):
+    locality = update.message.text  # число сзади
+    update.message.reply_text("Хорошо, сбор данных окончен.\n"
+                              "Вы можете начать пользоваться мной!")
     return ConversationHandler.END
 
 
@@ -39,8 +66,11 @@ def main():
         entry_points=[CommandHandler('start', start)],
 
         states={
+            0: [MessageHandler(Filters.text, null_response)],
             1: [MessageHandler(Filters.text, first_response)],
-            2: [MessageHandler(Filters.text, second_response)]
+            2: [MessageHandler(Filters.text, second_response)],
+            3: [MessageHandler(Filters.text, third_response)],
+            4: [MessageHandler(Filters.text, forth_response)]
         },
 
         fallbacks=[CommandHandler('stop', stop)]
