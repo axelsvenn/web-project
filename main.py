9dfe22bd510a321700e19afd3c60a89ff0d7109f
axelsvenn <mrr.axelsven@gmail.com>
2022-04-16 14:16:49 +0300

    start

---
+++ main.py
@@ -0,0 +1,23 @@
+from telegram.ext import Updater, MessageHandler, Filters
+from telegram.ext import CallbackContext, CommandHandler
+from settings import TOKEN
+
+
+def echo(update, context):
+    update.message.reply_text(f"Я получил сообщение {update.message.text}")
+
+
+def main():
+    updater = Updater(TOKEN, use_context=True)
+
+    dp = updater.dispatcher
+    text_handler = MessageHandler(Filters.text, echo)
+
+    dp.add_handler(text_handler)
+    updater.start_polling()
+
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
